2836e99ce97ac1118eb2346fbee28ace37e1c494
BALAJI POTHULA <balan.pothula@gmail.com>
2020-05-02 22:32:03 +0530

    Update compile-redis-on-ubuntu18.sh

--- compile-redis-on-ubuntu18.sh
+++ compile-redis-on-ubuntu18.sh
@@ -4,8 +4,12 @@
 # Date        : 02 May 2020,
 # Description : Installing / Compiling REDIS from Source Code.
 
+# starting redis server.
+#       redis-server $HOME/redis/conf/redis.conf
+# nohup redis-server $HOME/redis/conf/redis.conf &
+
 # connecting to redis server.
-# redis-cli -h 127.0.0.1 -p 6379 -a password PING
+# redis-cli -h 127.0.0.1 -p 6379 -a password PING Hi..!
 
 # exit immediately if a command exits with a non-zero exit status.
 set -e
@@ -36,12 +40,11 @@ rm   -rf $HOME/redis-stable.tar.gz                                              
 mv       $HOME/redis-stable $HOME/redissrc                                         && \
 cd       $HOME/redissrc                                                            && \
 make                                                                               && \
-make test                                                                          && \
 mkdir -p $HOME/redis                                                               && \
 make install PREFIX=$HOME/redis                                                    && \
 cd       $HOME                                                                     && \
-cp       $HOME/redissrc/redis.conf $HOME/redis/conf                                && \
-touch    $HOME/redis/{redis.log,redis.pid}                                         && \
+cp       $HOME/redissrc/redis.conf $HOME/redis/bin                                 && \
+touch    $HOME/redis/bin/{redis.log,redis.pid}                                     && \
 echo "export PATH=$HOME/redis/bin:$PATH" | tee -a $HOME/.bashrc $HOME/.profile     && \
 source   $HOME/.bashrc $HOME/.profile                                              && \
 rm   -rf $HOME/redissrc
